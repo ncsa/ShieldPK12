066af06f4cd420304d1a442e68de26c55285a88a
Chen Wang <cwang138@illinois.edu>
2021-01-07 15:05:54 -0600

    rewrite the tree; identifier using string

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def update_questions():
     if not current_nid:
         current_nid = testing_decision_tree.tree.root
     else:
-        current_nid = int(current_nid)
+        current_nid = current_nid
 
     option_nodes = testing_decision_tree.tree.children(current_nid)
     option_nodes_dict =[]
@@ -47,7 +47,7 @@ def update_questions():
 @app.route('/next', methods=['POST'])
 def next():
     if request.get_json() and request.get_json()['selected_nid']:
-        selected_nid = int(request.get_json()['selected_nid'])
+        selected_nid = request.get_json()['selected_nid']
         option_nodes = testing_decision_tree.tree.children((selected_nid))
         option_nodes_dict = []
         for node in option_nodes:
@@ -65,7 +65,7 @@ def next():
 @app.route('/prev', methods=['POST'])
 def prev():
     if request.get_json() and request.get_json()['current_nid']:
-        current_nid = int(request.get_json()['current_nid'])
+        current_nid = request.get_json()['current_nid']
         current = testing_decision_tree.tree.get_node(current_nid)
         option_nodes = testing_decision_tree.tree.children((current.bpointer))
         option_nodes_dict = []
@@ -84,7 +84,7 @@ def prev():
 @app.route("/submit", methods=['POST'])
 def submit():
     if request.get_json() and request.get_json()['submitted_nid']:
-        submitted_nid = int(request.get_json()['submitted_nid'])
+        submitted_nid = request.get_json()['submitted_nid']
         past_nodes = []
         for nid in testing_decision_tree.tree.rsearch(submitted_nid):
             # flip the order of nodes; parent - child
