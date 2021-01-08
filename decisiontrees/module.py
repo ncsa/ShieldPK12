4ca78d8865e537492291ff780b2dece9db2ecc48
Chen Wang <cwang138@illinois.edu>
2021-01-08 09:59:41 -0600

    add method to get a summary of past question and answers

--- decisiontrees/module.py
+++ decisiontrees/module.py
@@ -12,8 +12,6 @@ class Module:
                 return page
         raise ValueError("current page id: " + question_id + " cannot be found!")
 
-        return None
-
     def next_page(self, question_id, answer_id):
         next_page_id = None
         current_page = None
@@ -63,6 +61,41 @@ class Module:
         else:
             # reach the beginning
             return None
+
+    def get_all_past_questions_answers(self, qa_map):
+        """
+        given question and answer map get their details
+        :param qa_map: { questionID: answerID }
+        :return:
+        """
+        response = []
+        for question_id, answer_id in qa_map.items():
+            found_page = False
+            for page in self.module:
+
+                if page["questionID"] == question_id:
+                    found_page = True
+
+                    found_answer = False
+                    for answer in page["answers"]:
+
+                        if answer["answerID"] == answer_id:
+                            found_answer = True
+                            response.append({
+                                "question": page["question"],
+                                "questionDescription": page["description"],
+                                "questionResources": page["resources"],
+                                "answer": answer["answer"],
+                                "answerDescription": answer["description"],
+                                "answerResources": page["resources"]
+                            })
+                    if not found_answer:
+                        raise ValueError("answerID: " + answer_id + "not found!")
+
+            if not found_page:
+                raise ValueError("questionID: " + question_id + " not found!")
+
+        return response
 
 
 if __name__ == "__main__":
