d42f9e89b7bddbf6372eb3e7a5ce342c5097587f
Chen Wang <cwang138@illinois.edu>
2021-02-08 12:57:58 -0600

    landing page switch between start and resume

--- static/landing.js
+++ static/landing.js
@@ -1,7 +1,13 @@
-$("#regress").on("click", function (e){
-    var module = localStorage.getItem("module");
-    if (module !== null) {
-        e.preventDefault();
-        window.location.href = "/regress?module=" + module;
+// GET start or resume
+var modules = ["cleaning", "distancing", "data-infrastructure", "mask", "testing", "ventilation"];
+
+modules.forEach(function (module, index) {
+    let moduleQNA = localStorage.getItem(module);
+    if (moduleQNA !== null && JSON.parse(moduleQNA).length > 0)
+    {
+        $("#" + module).html("Resume <i class=\"fas fa-arrow-right\"></i>");
+    }
+    else{
+         $("#" + module).html("Start <i class=\"fas fa-arrow-right\"></i>");
     }
 });
