83eddf6cd0252899055c3a33bf5e3c10a3a2148f
Chen Wang <cwang138@illinois.edu>
2021-03-16 14:23:55 -0500

    add redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,18 @@
-#!bin/bash
+#!/bin/sh
 
 git checkout develop
-git pull
-docker-compose down
-docker-compose up --build -d
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo $(date) "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo $(date) "Need to pull"
+    git pull
+    docker-compose down
+    docker-compose up --build -d
+else
+    echo $(date) "Diverged"
+fi
