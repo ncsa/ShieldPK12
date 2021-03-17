3faeb2bd2f27a2b184dcebd1c9ad373b7a1dd866
Chen Wang <cwang138@illinois.edu>
2021-03-17 09:58:31 -0500

    further update script

--- redeploy.sh
+++ redeploy.sh
@@ -9,10 +9,8 @@ NEW_HEAD=$(git rev-parse HEAD)
 
 if [ $OLD_HEAD = $NEW_HEAD ]; then
     echo $(date) "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
+else
     echo $(date) "Need to redeploy"
     docker-compose down
     docker-compose up --build -d
-else
-    echo $(date) "Diverged"
 fi
